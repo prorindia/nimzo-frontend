3a2a40bf673778786bd11beeb933864635aea497
Shakti Chauhan <prorindia@gmail.com>
2026-01-01 20:51:00 +0530

    Fix API baseURL with /api

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL + "/api",
+  baseURL: process.env.REACT_APP_BACKEND_URL + "/api", // ✅ REQUIRED
 });
 
 API.interceptors.request.use(
